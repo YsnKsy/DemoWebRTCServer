ab430f415c301d722fb4c0e9209dd0406348b1ef
Yassine Kassy <yassine.kassy@mts-system.com>
2019-10-01 10:50:44 +0100

    code cleaning

--- index.js
+++ index.js
@@ -2,36 +2,30 @@ const WebSocketServer = require('websocket').server;
 const http = require('http');
 const server = http.createServer().listen(1337);
 const wsServer = new WebSocketServer({ httpServer: server });
-
 const clients = {};
 const peer = (peerID = null, description = { type: null, sdp: null }) => ({ peerID, description });
 const peers = { offer: peer(), answer: peer() };
 const extractUserID = req => req.resource.replace(/\//g, '');
+const isValidSDP = ({ type, sdp }) => (type === 'offer' || type === 'answer') && (sdp && sdp.length);
 
 // WebSocket server
 wsServer.on('request', request => {
     const connection = request.accept(null, request.origin);
     const userID = extractUserID(request);
     clients[userID] = connection;
-
     clients[userID].on('message', message => {
         const { type, utf8Data } = message;
         if (type === 'utf8') {
-            const { type, sdp } = JSON.parse(utf8Data);
-            const validReq = (type === 'offer' || type === 'answer') && (sdp && sdp.length);
-            if (validReq) {
-                peers[type] = peer(userID, { type, sdp });
+            const data = JSON.parse(utf8Data);
+            if (isValidSDP(data)) {
+                peers[data.type] = peer(userID, data);
                 Object.entries(clients)
                     .filter(c => c[0] !== userID)
-                    .map(c => c[1].send(JSON.stringify(peers[type].description)));
+                    .map(c => c[1].send(JSON.stringify(peers[data.type].description)));
             }
         }
     });
 
     clients[userID].on('close', () => delete clients[userID]);
-
-    clients[userID].on('exchange', (data) => {
-        console.log('exchange', data);
-    });
+    clients[userID].on('exchange', (data) => console.log('exchange', data));
 });
-
