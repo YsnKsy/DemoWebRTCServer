e2d58c90dcf06c14d62e48f65529a19b1675f004
Yassine Kassy <yassine.kassy@mts-system.com>
2019-10-02 18:00:20 +0100

    Ready

--- index.js
+++ index.js
@@ -3,11 +3,27 @@ const http = require('http');
 const server = http.createServer().listen(1337);
 const wsServer = new WebSocketServer({ httpServer: server });
 const clients = {};
-const peer = (peerID = null, description = { type: null, sdp: null }) => ({ peerID, description });
+
+const peer = (description = { type: null, sdp: null }) => description;
 const peers = { offer: peer(), answer: peer() };
+
 const extractUserID = req => req.resource.replace(/\//g, '');
+const isEmptyPeer = (p, t) => (p[t].type === null);
 const isValidSDP = ({ type, sdp }) => (type === 'offer' || type === 'answer') && (sdp && sdp.length);
-const isEmptyPeer = (p, t) => (p[t].description.type === null);
+const isValidCandidate = (type, { candidate, sdpMLineIndex, sdpMid }) => (type === 'candidate') && (typeof candidate === 'string' && typeof sdpMLineIndex === 'number' && typeof sdpMid === 'string');
+
+/*
+{
+    sdp:'v=0\r\no=- 3535576052355586379 2 IN IP4 127.0.0.1\r\ns=-\r\nt=0 0\r\na=group:BUNDLE data\r\na=msid-semantic: WMS\r\nm=application 9 DTLS/SCTP 5000\r\nc=IN IP4 0.0.0.0\r\na=ice-ufrag:sfu5\r\na=ice-pwd:1xespOa2tDgcza4ZBgPgKcfP\r\na=ice-options:trickle renomination\r\na=fingerprint:sha-256 75:A8:A0:72:F2:B0:B9:D0:99:0B:2F:05:AE:5D:D2:95:42:BA:E2:AA:F5:22:E4:0F:87:D4:E8:A9:0B:8E:92:FC\r\na=setup:actpass\r\na=mid:data\r\na=sctpmap:5000 webrtc-datachannel 1024\r\n',
+    type: 'offer' 
+}
+------
+{
+    candidate: 'candidate:2943013415 1 udp 2122260223 192.168.2.3 52456 typ host generation 0 ufrag sfu5 network-id 1 network-cost 10',
+    sdpMLineIndex: 0,
+    sdpMid: 'data'
+}
+*/
 
 // WebSocket server
 wsServer.on('request', request => {
@@ -17,16 +33,21 @@ wsServer.on('request', request => {
     clients[userID].on('message', message => {
         const { type, utf8Data } = message;
         if (type === 'utf8') {
-            const data = JSON.parse(utf8Data);
-            if (isValidSDP(data)) {
-                peers[data.type] = isEmptyPeer(peers, data.type) ? peer(userID, data) : peers[data.type];
-                Object.entries(clients)
-                    .filter(c => c[0] !== userID)
-                    .map(c => c[1].send(JSON.stringify(peers[data.type].description)));
+            const { type, payload } = JSON.parse(utf8Data);
+            let result = null;
+            
+            if (isValidSDP(payload)) {
+                peers[type] = isEmptyPeer(peers, type) ? peer(payload) : peers[type];
+                result = { type, payload: peers[type] };
+            } else if (isValidCandidate(type, payload)) {
+                result = { type, payload };
             }
+
+            console.log('result', result);
+
+            if (result !== null) Object.entries(clients).filter(c => c[0] !== userID).map(c => c[1].send(JSON.stringify(result)));
         }
     });
-
     clients[userID].on('close', () => delete clients[userID]);
     clients[userID].on('exchange', (data) => console.log('exchange', data));
 });
